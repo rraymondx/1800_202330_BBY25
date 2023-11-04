a0dc75d1ec0c791b662691ef1af1f1e7069ff0dc
LordWilliam <willjordan05@icloud.com>
2023-11-03 19:56:59 -0700

    user information is written to the firestore database upon login.

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,22 +1,40 @@
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+// ----------------
+// HowRu ui config.
+// ----------------
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-      // User successfully signed in.
-      return true;
-    },
-    uiShown: function () {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
+      //--------------------------------------------------------------
+      // Generates Users in the Firestore database.
+      // Records their name, email, country and location (random atm).
+      //--------------------------------------------------------------
+      var user = authResult.user;
+      if (authResult.additionalUserInfo.isNewUser) {
+          db.collection("users").doc(user.uid).set({
+                 name: user.displayName,
+                 email: user.email,
+                 country: "Canada",
+                 location: Math.random(100)
+          }).then(function () {
+                 console.log("New user added to firestore");
+                 window.location.assign("main.html");
+          }).catch(function (error) {
+                 console.log("Error adding new user: " + error);
+          });
+      } else {
+          return true;
+      }
+          return false;
+      },
   },
   signInFlow: 'popup',
   signInSuccessUrl: "./main.html",
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  signInOptions: [ {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    }
   ],
   // Terms of service url.
   tosUrl: '<your-tos-url>',
