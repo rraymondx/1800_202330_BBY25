3ec1e289492be6ec9507f0cbd895fec2c514563a
LordWilliam <willjordan05@icloud.com>
2023-11-03 21:13:07 -0700

    Completed the temporary implementation of user collection. User information is stored to the firestore db upon login/signup. User information is read to display their userName on the main.html page within the welcome message. Their userName is also read and written into the brand new post card on the main page. For the time being, this page takes the user to the space page implemented by Bavneetlast sprint. Decent changes to the styling of the webpage in style.css (the master style sheet).

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+// --------------------------------------------------------
+// Populates the name-goes-here elements with the userName.
+// --------------------------------------------------------
+function populateNameClassElements(userName) {
+    var nameElements = document.getElementsByClassName('name-goes-here');
+    for (let i = 0; i < nameElements.length; i++) {
+        nameElements[i].innerHTML = userName;
+    }
+}
+
 function getNameFromAuth() {
   firebase.auth().onAuthStateChanged(user => {
         // ----------------------------
@@ -6,10 +16,10 @@ function getNameFromAuth() {
         if (user) {
             currentUser = db.collection("users").doc(user.uid);
             currentUser.get().then(userDoc => {
-                // Retrieve the users user name
+                // Retrieve the users userName
                 var userName = userDoc.data().name;
                 // Insert userName into the page
-                document.getElementById("name-goes-here").innerText = userName;
+                populateNameClassElements(userName);
             })
         } else {
             document.querySelector("#name-goes-here").innerText = "Anon?";
