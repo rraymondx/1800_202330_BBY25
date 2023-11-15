839750081b287ab0230dae2f500f03dd581a98dd
Raymond Xie <rraymondxie@gmail.com>
2023-11-14 17:41:35 -0800

    progress: created and restored conversation collection system

--- scripts/conversations.js
+++ scripts/conversations.js
@@ -7,65 +7,70 @@ var currentUser;
 // --------------------------------
 // Gets the ID of the current user.
 // --------------------------------
-// function getUserId() {
-//     firebase.auth().onAuthStateChanged(user => {
-//         // Check if user is signed in:
-//         if (user) {
-//             currentUser = user.uid;
-//             retriveUserConvos();
-//         } else {
-//             // No user is signed in.
-//             console.log("No user is signed in");
-//         }
-//     });
-// }
+function getUserId() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+            currentUser = user.uid;
+            retriveUserConvos();
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+        }
+    });
+}
 
-// function generateConvoName() {
-//     let id = "Convo-";
-//     let idNum = Math.random(100000000000);
+// -------------------
+// Get the other user.
+// -------------------
+function getOtherUserName(convo, i) {
+    let otherUser;
 
-//     return id + idNum;
-// }
+    convo.data().Users;
+    for (let i = 0; i < convo.data().Users.length; i++) {
+        if (convo.data().Users[i] != currentUser) {
+            otherUser = convo.data().Users[i];
+        }
+    }
+
+    let otherUserP = db.collection("users").doc(otherUser);
+
+    otherUserP.get().then((doc) => {
+        //console.log(doc.data().name);
+        return doc.data().name;
+    });
+}
+
+// --------------------
+// Generate convo list.
+// --------------------
+function generateCard(convo, i) {
+    let conversationTemplate;
+    let conversation;
+
+    //let conversationList = document.getElementById();
+    let otherUser = getOtherUserName(convo, i);
+    console.log(otherUser);
+}
 
 // ---------------------------------------------------------
 // Gets the message chains that the user is involved in and
 // Puts them into a new collection.
 // Backwards thinking, we need to get users from reply
 // ---------------------------------------------------------
-
-/*
 function retriveUserConvos() {
-    let totalConversations = db.collection("conversations");
+    let totalConversations = db.collection("conversations"); // List of message chains
+    let userConversations = totalConversations.where("Users", "array-contains", currentUser); // Message chains that involve the user.
 
-    const q = totalConversations.where("Users", "array-contains", currentUser)
+    userConversations.get().then((doc) => {
+        let involvedConvos = doc.docs;
 
-    q.get()
-        .then(snapshot => {
-            // If the query returned multiple documents, you need to loop through them
-            snapshot.forEach(doc => {
-                let theUsers = (doc.id, " => ", doc.data().Users);
+        for (let i = 0; i < involvedConvos.length; i++) {
+            generateCard(involvedConvos[i], i);
+        }
+    });
 
-                let conversationName = "c=" + theUsers[0] + "=" + theUsers[1];
-                console.log(conversationName);
+}
 
-                if (!doc.exists()) {
-                    db.collection("userConversations").doc(conversationName).set({
-                        users: theUsers
-                    }).then(function (){
-                        console.log("New Document");
-                    }).catch(function (error) {
-                        console.log("Error occured" + error);
-                    })
-                }
-            });
-        })
-        .catch(error => {
-            // It's important to catch any errors that occur during the get
-            console.error("Error getting documents: ", error);
-        });
-} 
+getUserId();
 
-Code is useless btw.
-
-*/
-// getUserId();
