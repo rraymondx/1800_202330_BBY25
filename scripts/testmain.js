d79cd99b84d75ff0e54a43315b4f13911dc541a3
BavneetDhillon  <bavneetsdhillon@icloud.com>
2023-11-16 11:36:19 -0800

    Added layer to map to show other user locations

--- scripts/testmain.js
+++ scripts/testmain.js
@@ -29,45 +29,106 @@ function getNameFromAuth() {
 
 getNameFromAuth(); //run the function  
 
-  document.addEventListener('DOMContentLoaded', function() {
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            const docRef = db.collection('users').doc(user.uid);
-            docRef.get().then(function(doc) {
-                if (doc.exists) {
-                    const geoPoint = doc.data().location;
-                    const userLocation = {
-                        lat: geoPoint.latitude,
-                        lng: geoPoint.longitude
-                    };
-                    // Use this location in Mapbox
-                    addUserLocationToMap(userLocation);
-                } else {
-                    console.log('No such document!');
-                }
-            }).catch(function(error) {
-                console.error('Error getting document:', error);
-            });
-        } else {
-            // No user is signed in
-            console.log('No user is signed in.');
-        }
+//   document.addEventListener('DOMContentLoaded', function() {
+//     firebase.auth().onAuthStateChanged(function(user) {
+//         if (user) {
+//             const docRef = db.collection('users').doc(user.uid);
+//             docRef.get().then(function(doc) {
+//                 if (doc.exists) {
+//                     const geoPoint = doc.data().location;
+//                     const userLocation = {
+//                         lat: geoPoint.latitude,
+//                         lng: geoPoint.longitude
+//                     };
+//                     // Use this location in Mapbox
+//                     addUserLocationToMap(userLocation);
+//                 } else {
+//                     console.log('No such document!');
+//                 }
+//             }).catch(function(error) {
+//                 console.error('Error getting document:', error);
+//             });
+//         } else {
+//             // No user is signed in
+//             console.log('No user is signed in.');
+//         }
+//     });
+// });
+
+function addUserLocationsToMap(map) {
+
+    // Reading information from "users" collection in Firestore
+    db.collection('users').get().then(allUsers => {
+        const features = []; // Defines an empty array for user locations
+
+        allUsers.forEach(doc => {
+            // Check if the user has valid location data
+            if (doc.exists && doc.data().location != null && !isNaN(doc.data().location.latitude) && !isNaN(doc.data().location.longitude)) {
+                // Extract coordinates of the user
+                const coordinates = [doc.data().location.longitude, doc.data().location.latitude];
+                console.log(coordinates);
+                // Extract other fields you might want, e.g., username
+                const userName = doc.data().name; // User's Name
+
+                // Push user information (properties, geometry) into the features array
+                features.push({
+                    'type': 'Feature',
+                    'properties': {
+                        'description': `<strong>${userName}</strong>`
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': coordinates
+                    }
+                });
+            }
+        });
+
+        // Adds user locations as features to the map
+        map.addSource('user-locations', {
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': features
+            }
+        });
+
+        // Creates a layer above the map displaying the user locations
+        map.addLayer({
+            'id': 'user-locations',
+            'type': 'circle', // Visual style of the markers
+            'source': 'user-locations',
+            'paint': {
+                'circle-color': '#ff6347', // Customize the color
+                'circle-radius': 6,
+                'circle-stroke-width': 2,
+                'circle-stroke-color': '#ffffff'
+            }
+        });
+
+        // Add interactivity for the user location markers
+        map.on('click', 'user-locations', (e) => {
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const description = e.features[0].properties.description;
+
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+        });
+
+        map.on('mouseenter', 'user-locations', () => {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+
+        map.on('mouseleave', 'user-locations', () => {
+            map.getCanvas().style.cursor = '';
+        });
     });
-});
-
-function addUserLocationToMap(userLocation) {
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [userLocation.lng, userLocation.lat],
-        zoom: 8.8
-    });
-
-    new mapboxgl.Marker()
-        .setLngLat([userLocation.lng, userLocation.lat])
-        .addTo(map);
-
-    map.flyTo({ center: [userLocation.lng, userLocation.lat] });
 }
 
 function showMap() {
