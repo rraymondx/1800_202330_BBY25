18e004d49a0f3b76ce956f4d229c9b6ea8c4f350
Raymond Xie <rraymondxie@gmail.com>
2023-11-21 20:16:46 -0800

    main.html ui fix with the top banner

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -6,66 +6,66 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // -----------------------------------------------
 function getUserIcon() {
     let potentialIcons = [
-        'llama1.jpg', 'llama2.jpg', 'llama3.jpg', 'llama4.jpg', 'llama5.jpg', 
-        'llama6.jpg', 'llama7.jpg', 'llama8.jpg', 'llama9.jpg', 'llama10.jpg', 
+        'llama1.jpg', 'llama2.jpg', 'llama3.jpg', 'llama4.jpg', 'llama5.jpg',
+        'llama6.jpg', 'llama7.jpg', 'llama8.jpg', 'llama9.jpg', 'llama10.jpg',
         'llama11.jpg', 'llama12.jpg', 'llama13.jpg', 'llama14.jpg', 'llama15.jpg'];
 
-    let randomNumber = Math.floor(Math.random() * (14 - 0 + 1) + 1);    
+    let randomNumber = Math.floor(Math.random() * (14 - 0 + 1) + 1);
     return potentialIcons[randomNumber];
 }
+
+// document.getElementById("auth-container").innerHTML = "<h1> Loading </h1>";
 
 // ----------------
 // HowRu ui config.
 // ----------------
 var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-        var user = authResult.user;
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var userLocation = new firebase.firestore.GeoPoint(position.coords.latitude, position.coords.longitude);
-    
-            if (authResult.additionalUserInfo.isNewUser) {
-                db.collection("users").doc(user.uid).set({
-                    name: user.displayName || "Unknown", 
-                    email: user.email,
-                    country: "Canada", 
-                    location: userLocation,
-                    picture: "" + getUserIcon()
-                }, { merge: true }).then(function () { 
-                    window.location.assign("main.html");
-                }).catch(function (error) {
-                    console.log("Error updating user data: " + error);
-                });       
-            } else {
-                db.collection("users").doc(user.uid).set({
-                    location: userLocation,
-                }, { merge: true }).then(function () { 
-                    window.location.assign("main.html");
-                }).catch(function (error) {
-                    console.log("Error updating user data: " + error);
-                });  
-            }
-        }, function (error) {
-            console.warn(`ERROR(${error.code}): ${error.message}`);
-            
-        });
-    
-        return false; 
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            document.getElementById("auth-container").innerHTML = "<h1> Loading </h1>";
+            var user = authResult.user;
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var userLocation = new firebase.firestore.GeoPoint(position.coords.latitude, position.coords.longitude);
+                if (authResult.additionalUserInfo.isNewUser) {
+                    db.collection("users").doc(user.uid).set({
+                        name: user.displayName || "Unknown",
+                        email: user.email,
+                        country: "Canada",
+                        location: userLocation,
+                        picture: "" + getUserIcon()
+                    }, { merge: true }).then(function () {
+                        window.location.assign("main.html");
+                    }).catch(function (error) {
+                        console.log("Error updating user data: " + error);
+                    });
+                } else {
+                    db.collection("users").doc(user.uid).set({
+                        location: userLocation,
+                    }, { merge: true }).then(function () {
+                        window.location.assign("main.html");
+                    }).catch(function (error) {
+                        console.log("Error updating user data: " + error);
+                    });
+                }
+            }, function (error) {
+                console.warn(`ERROR(${error.code}): ${error.message}`);
+
+            });
+
+            return false;
+        },
+
     },
-    
-    
-  
-  },
-  signInFlow: 'popup',
-  signInSuccessUrl: "./main.html",
-  signInOptions: [ {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    signInFlow: 'popup',
+    signInSuccessUrl: "./main.html",
+    signInOptions: [{
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
     }
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>',
-  // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
 };
 
 ui.start('#firebaseui-auth-container', uiConfig);
