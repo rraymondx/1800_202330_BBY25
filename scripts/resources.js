a65660201b10446cb99d8b94f91fc069be0f69b4
Raymond Xie <rraymondxie@gmail.com>
2023-11-23 22:11:50 -0800

    resources pages styles for UI finished, still need to add links

---
+++ scripts/resources.js
@@ -0,0 +1,15 @@
+document.getElementById("r-card-1").addEventListener('click', () => {
+    window.location.href = "...";
+}, false)
+
+document.getElementById("r-card-2").addEventListener('click', () => {
+    window.location.href = "...";
+}, false)
+
+document.getElementById("r-card-3").addEventListener('click', () => {
+    window.location.href = "...";
+}, false)
+
+document.getElementById("r-card-4").addEventListener('click', () => {
+    window.location.href = "...";
+}, false)
