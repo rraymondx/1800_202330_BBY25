b535ab52d3c3114a5497965eafd179c2fe2c288c
ray <rraymondxie@gmail.com>
2023-11-24 19:31:54 -0800

    generated images, applied images, and styled for resources.html

--- scripts/resources.js
+++ scripts/resources.js
@@ -1,15 +1,15 @@
 document.getElementById("r-card-1").addEventListener('click', () => {
-    window.location.href = "...";
+    window.location.href = "https://talksuicide.ca/";
 }, false)
 
 document.getElementById("r-card-2").addEventListener('click', () => {
-    window.location.href = "...";
+    window.location.href = "https://www.wellnesstogether.ca/en-ca/";
 }, false)
 
 document.getElementById("r-card-3").addEventListener('click', () => {
-    window.location.href = "...";
+    window.location.href = "https://www2.gov.bc.ca/gov/content/health/managing-your-health/mental-health-substance-use";
 }, false)
 
 document.getElementById("r-card-4").addEventListener('click', () => {
-    window.location.href = "...";
+    window.location.href = "https://mentalhealthcommission.ca/resources/";
 }, false)
